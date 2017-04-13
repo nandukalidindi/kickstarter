acd33aa8a21a05a6ef0de6e609abb43f1bc0cf65
nandukalidindi <nandukalidindi@gmail.com>
2017-04-13 01:42:33 -0400

    Add mock seeder classes to seed the database for SQL query testing

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,59 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(ProjectSeeder::class);
+        $this->call(CreditCardSeeder::class);
+        $this->call(PledgeSeeder::class);
+        $this->call(ReviewSeeder::class);
+        $this->call(FollowerSeeder::class);
     }
+}
+
+class UserSeeder extends Seeder
+{
+  public void run()
+  {
+
+  }
+}
+
+class ProjectSeeder extends Seeder
+{
+  public void run()
+  {
+
+  }
+}
+
+class CreditCardSeeder extends Seeder
+{
+  public void run()
+  {
+
+  }
+}
+
+class PledgeSeeder extends Seeder
+{
+  public void run()
+  {
+
+  }
+}
+
+class ReviewSeeder extends Seeder
+{
+  public void run()
+  {
+
+  }
+}
+
+class FollowerSeeder extends Seeder
+{
+  public void run()
+  {
+
+  }
 }
