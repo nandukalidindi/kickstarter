9221564e8a874da98b704370cbe8d27ae6bd6c99
nandukalidindi <nandukalidindi@gmail.com>
2017-04-29 17:37:10 -0400

    Add login functionality with validation message

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,15 +1,24 @@
 class LoginController < ApplicationController
 
   def login
-
+    
   end
 
   def authenticate
-    binding.pry
+    email = params[:email]
+    password = params[:password]
+
+    count = ActiveRecord::Base.connection.execute("SELECT COUNT(*) FROM users where email='#{email}' AND password='#{password}'").first["count"]
+
+    if count == "1"
+      redirect_to '/'
+    else
+      redirect_to '/login', alert: 'The email address and password you entered do not match.'
+    end
   end
 
   def signup
-    
+
   end
 
   def create_user
