e487e635fa1a5a12ada1d6950c792fa192ee68e3
nandukalidindi <nandukalidindi@gmail.com>
2017-04-29 14:54:37 -0400

    Project listing page to get records from database rather than from the copied source

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,6 +1,15 @@
 class ProjectController < ApplicationController
 
   def index
+    # binding.pry
+    projects = ActiveRecord::Base.connection.execute('SELECT * FROM projects')
+    @projects = [[], [], [], [], [], [], []]
+
+    projects.each_with_index do |project, index|
+      @projects[index/4] << project
+    end
+
+    # binding.pry
   end
 
   def new
